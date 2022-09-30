9d7024bda87c50fe04122482db6690db422a12dc
dzhunkoffski <dzhunkofffski@gmail.com>
2022-09-30 14:39:48 +0300

    g

--- data/config.py
+++ data/config.py
@@ -1,10 +1,6 @@
 # pylint: disable=missing-module-docstring
-
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-mToken = os.environ['BOT_TOKEN']
+# load_dotenv()
+# mToken = os.environ['BOT_TOKEN']
 
 admin_ids = [
     474335162,
